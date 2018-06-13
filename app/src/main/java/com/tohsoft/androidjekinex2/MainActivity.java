d62c8c2c1805e717c3fc85590ec159c9816bbdd5
Haodv <haodv@tohsoft.com>
2018-06-13 15:09:45 +0700

    update new feature, for test release, debug build apk

--- app/src/main/java/com/tohsoft/androidjekinex2/MainActivity.java
+++ app/src/main/java/com/tohsoft/androidjekinex2/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showToast(BuildConfig.DEBUG ? "is debug" : " realse");
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
