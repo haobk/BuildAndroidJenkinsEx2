7b0e7c568221c11e1ab3899a4f77ba1fb993ba02
Haodv <haodv@tohsoft.com>
2018-06-13 11:06:36 +0700

    init code, build Android with Jenkins ex2

---
+++ app/src/main/java/com/tohsoft/androidjekinex2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.tohsoft.androidjekinex2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Test jenkins 2");
+            }
+        });
+    }
+    private void showToast(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+}
