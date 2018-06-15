8007f6c1fa62462b25ff027794433b32233009ee
Haodv <haodv@tohsoft.com>
2018-06-15 10:43:22 +0700

    update new feature

--- app/src/main/java/com/tohsoft/androidjekinex2/MainActivity.java
+++ app/src/main/java/com/tohsoft/androidjekinex2/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((TextView)findViewById(R.id.app_id)).setText(BuildConfig.APPLICATION_ID);
+        ((TextView)findViewById(R.id.app_id)).setText(BuildConfig.APPLICATION_ID + "|" + BuildConfig.BUILD_TYPE);
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
